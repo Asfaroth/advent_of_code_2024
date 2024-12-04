6c1dd131a74bceb709f1fc34a2ac9088afa942d7
Asfaroth <83340199+Asfaroth@users.noreply.github.com>
2024-12-04 20:17:22 +0100

    Day 4 part two

--- src/aoc_2024/day4.clj
+++ src/aoc_2024/day4.clj
@@ -21,8 +21,24 @@
         reverseDiagonals (reduce + (map
                                     #(countOccurences (matrix/diagonal reverseInputMatrix %))
                                     (range (* (matrix/row-count reverseInputMatrix) -1) (matrix/column-count reverseInputMatrix))))]
-    (println [horizontal vertical diagonals reverseDiagonals])
     (+ horizontal vertical diagonals reverseDiagonals)))
 
+(defn check3x3Matrix
+  "Returns either 1 if an X-MAS structure is found or 0 if not."
+  [matrix]
+  (if (and
+       (re-matches #"(MAS)|(SAM)" (apply str (matrix/diagonal matrix)))
+       (re-matches #"(MAS)|(SAM)" (apply str (matrix/diagonal (map reverse matrix)))))
+    1 0))
+
+(defn testMatrix
+  ([matrix] (testMatrix matrix 0 0 0))
+  ([matrix xPos yPos occurences]
+   (if (< xPos (- (matrix/column-count matrix) 3))
+     (recur matrix (inc xPos) yPos (+ (check3x3Matrix (matrix/submatrix matrix xPos 3 yPos 3)) occurences))
+     (if (< yPos (- (matrix/row-count matrix) 3))
+       (recur matrix 0 (inc yPos) (+ (check3x3Matrix (matrix/submatrix matrix xPos 3 yPos 3)) occurences))
+       (+ (check3x3Matrix (matrix/submatrix matrix xPos 3 yPos 3)) occurences)))))
+
 (defn twoStar []
-  0)
+  (testMatrix inputMatrix))
